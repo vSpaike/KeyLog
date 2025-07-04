d4a930d5ae63ccbd98e276cae1d8605c0fdd443f
vSpaike <stryckeurs75@gmail.com>
2025-07-04 16:49:25 +0200

    add: nouveau test mais pas concluant

--- project/faisonlemain.py
+++ project/faisonlemain.py
@@ -1,3 +1,17 @@
-import pywin32 as pw
+import msvcrt
 
-word = pw
+"""
+récupère touche seulement dans le terminal 
+"""
+
+l_keys = []
+cpt=0
+while True:
+    cpt+=1
+    if msvcrt.kbhit():
+        key = msvcrt.getch()
+        l_keys.append(key)
+        # print(f'Key pressed: {key}')
+        if cpt > 2:
+            cpt=0
+            print(l_keys) 
